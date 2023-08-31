398ec0f8a05c4151cfb2b6e1316a537609e469e4
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-08-31 16:17:46 +0000

    task 18..

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,10 +10,10 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *parent, *grand_parent;
 
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
 	parent = node->parent;
 	grand_parent = parent->parent;
-	if (node == NULL || parent == NULL || grand_parent == NULL)
-		return (NULL);
 	if (parent == grand_parent->left && grand_parent->right != NULL)
 		return (grand_parent->right);
 	else if (parent == grand_parent->right && grand_parent->left != NULL)
