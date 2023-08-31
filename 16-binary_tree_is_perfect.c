61b06a7c964c76876d93762984104511729ecdae
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-08-31 15:12:40 +0000

    t16..

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -20,21 +20,33 @@ int measure_height(const binary_tree_t *tree)
 		return (right + 1);
 }
 /**
- * check_nodes - checks if node has 2 children or not
+ * check_nodes - checks if node has 2 children and if leaves in right level
  *
  * @tree: pointer to the root node of the tree
+ * @height: height of the tree
+ * @level: level of tree
  *
  * Return: 1 in case true
  */
-int check_nodes(const binary_tree_t *tree)
+int check_nodes(const binary_tree_t *tree, int height, int level)
 {
 	if (tree == NULL)
 		return (0);
 	if ((tree->left != NULL && tree->right == NULL) ||
 			(tree->right != NULL && tree->left == NULL))
+	{
 		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		if (level != height)
+		{
+			return (0);
+		}
+	}
 	if (tree->left && tree->right)
-		return (measure_height(tree->left) && measure_height(tree->right));
+		return (check_nodes(tree->left, height, level + 1) &&
+				check_nodes(tree->right, height, level + 1));
 	else
 		return (1);
 }
@@ -47,13 +59,14 @@ int check_nodes(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int height_left, height_right, checker;
+	int height, height_left, height_right, checker, level = 1;
 
 	if (tree == NULL)
 		return (0);
 	height_left = measure_height(tree->left);
 	height_right = measure_height(tree->right);
-	checker = check_nodes(tree);
+	height = measure_height(tree);
+	checker = check_nodes(tree, height, level);
 	if (height_left == height_right && checker == 0)
 		return (0);
 	if (height_left != height_right)
